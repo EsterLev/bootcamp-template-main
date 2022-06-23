7dc942ed7ff6d2aec8bb3aa53a8fd6924facc7d2
EstiLev <st3281357@gmail.com>
2022-06-23 15:14:09 +0300

    Search Func

--- src/index.js
+++ src/index.js
@@ -3,34 +3,106 @@ class User {
 
     printUser = document.querySelector('.ShowUser');
 
-    #UserName;
-    #UserWeight;
+    #firstName;
+    #lastName;
+    #city;
+    #street;
+    #number;
+    #phone;
+    #email;
+    #height;
+    #weight;
     #id;
     static Id = 0;
     //function
     //GET and SET
     //for the private variables
-    set setUserName(userName) {
-        this.#UserName = userName;
+    set setFirstName(firstName) {
+        this.#firstName = firstName;
     }
-    get getUserName() {
-        return this.#UserName;
+
+    get getFirstName() {
+        return this.#firstName;
     }
-    set setUserWeight(UserWeight) {
-        this.#UserWeight = UserWeight;
+
+    set setLastName(LastName) {
+        this.#lastName = LastName;
     }
-    get getUserWeight() {
-        return this.#UserWeight;
+    get getLastName() {
+        return this.#lastName;
+    }
+
+    get getPhone() {
+        return this.#phone;
+    }
+
+    set setPhone(phone) {
+        this.#phone = phone;
+    }
+    get getPhone() {
+        return this.#phone;
+    }
+
+    set setEmail(mail) {
+        this.#email = mail;
+    }
+    get getEmail() {
+        return this.#email;
+    }
+
+    set setCity(city) {
+        this.#city = city;
+    }
+    get getCity() {
+        return this.#city;
+    }
+
+    set setStreet(street) {
+        this.#street = street;
+    }
+    get getStreet() {
+        return this.#street;
+    }
+
+    set setNumber(number) {
+        this.#number = number;
+    }
+    get getNumber() {
+        return this.#number;
+    }
+
+    set setHeight(height){
+        this.#height = height;
+    }
+
+    get getHeight(){
+        return this.#height;
+    }
+
+    set setWeight(weight){
+        this.#weight = weight;
+    }
+
+    get getWeight(){
+        return this.#weight;
     }
 
     get getId() {
         return this.#id;
     }
 
-    constructor(userName, UserWeight) {
-        this.#UserName = userName;
+
+    constructor(firstName, lastName, city, street, number, phone, email, height, weight) {
+        this.#firstName = firstName;
+        this.#lastName = lastName;
+        this.#city = city;
+        this.#street = street;
+        this.#number = number;
+        this.#phone = phone;
+        this.#email = email;
+        this.#height = height;
+        this.weight = weight;
         this.#id = ++this.Id;
-        this.#UserWeight = UserWeight;
     };
 
     ShowUser = (user) => {
@@ -127,14 +199,33 @@ class Manager {
     AddUser(user) {
         return usersList.push(user);
     }
-
+    u = false;
     ShowFilterUsers(user) {
-        u = this.#filterUsers.forEach(us => {
-            // if (us === user) {
-            //     break;
-            // }
+        if(this.#filterUsers!==undefined){
+            this.#filterUsers.foreach(us => {
+                if(us.id === user.id)
+                    u = true;
+           });
+            if (u === false)
+                this.#filterUsers.push(user);
+        }
+        else{
+            this.#filterUsers.push(user);
+        }
+    }
+
+    printUsersFilter() {
+        let table = '';
+        this.#filterUsers.forEach(user => {
+            console.log(user);
+            table += `
+         <tr>
+             <th>${user.firstName + ' ' + user.lastName}</th>
+             <th>${user.weight[usersList.users.length - 1] / Math.sqrt(user.height)}</th><br/>
+         </tr>`
         })
-        this.#filterUsers.push(user);
+        const container = document.querySelector('.ShowUser');
+        container.innerHTML += table;
     }
 
     printUsers() {
@@ -151,44 +242,43 @@ class Manager {
         container.innerHTML += table;
     }
 
-    search(val) {
-        search = document.querySelector("#search");
-        searchBtn = document.querySelector("#searchBtn");
+    Search = (val)=> {
+        //search = document.querySelector("#search");
         usersList.users.forEach(user => {
-            console.log(user);
             if (user.firstName === val) {
-                newUser = new User(user.firstName, user.lastName);
-                newUser.ShowFilterUsers(user);
+                console.log(user);
+                //newUser = new User(user.firstName, user.lastName, user.city, user.street, user.number, user.phone, user.email, user.height, user.weight);
+                this.ShowFilterUsers(user);
             }
             if (user.lastName === val) {
-                newUser = new User(user.firstName, user.lastName);
+                newUser = new User(user.firstName, user.lastName, user.city, user.street, user.number, user.phone, user.email, user.height, user.weight);
                 newUser.ShowFilterUsers(user);
             }
             if (user.email === val) {
-                newUser = new User(user.firstName, user.lastName, user.email);
+                newUser = new User(user.firstName, user.lastName, user.city, user.street, user.number, user.phone, user.email, user.height, user.weight);
                 newUser.ShowFilterUsers(user);
             }
             if (user.phone === val) {
-                newUser = new User(user.firstName, user.lastName, user.phone);
+                newUser = new User(user.firstName, user.lastName, user.city, user.street, user.number, user.phone, user.email, user.height, user.weight);
                 newUser.ShowFilterUsers(user);
             }
             if (user.address.city === val) {
-                newUser = new User(user.firstName, user.lastName, user.address);
+                newUser = new User(user.firstName, user.lastName, user.city, user.street, user.number, user.phone, user.email, user.height, user.weight);
                 newUser.ShowFilterUsers(user);
             }
             if (user.address.street === val) {
-                newUser = new User(user.firstName, user.lastName, user.address);
+                newUser = new User(user.firstName, user.lastName, user.city, user.street, user.number, user.phone, user.email, user.height, user.weight);
                 newUser.ShowFilterUsers(user);
             }
             if (user.address.number === val) {
-                newUser = new User(user.firstName, user.lastName, user.address);
+                newUser = new User(user.firstName, user.lastName, user.city, user.street, user.number, user.phone, user.email, user.height, user.weight);
                 newUser.ShowFilterUsers(user);
             }
             if (user.address === val) {
-                newUser = new User(user.firstName, user.lastName, user.address);
+                newUser = new User(user.firstName, user.lastName, user.city, user.street, user.number, user.phone, user.email, user.height, user.weight);
                 newUser.ShowFilterUsers(user);
+                newUser.printUsersFilter();
             }
-            this.printUsers();
         }
         )
     }
@@ -205,8 +295,12 @@ const showAll = document.querySelector('#showAllUsers')
 const searchBtn = document.querySelector("#searchBtn");
 
 const firstNameSearch = document.querySelector('#firstNameSearch');
+const firstName = document.querySelector('#firstName');
 const idSearch = document.querySelector('#idSearch');
+const id = document.querySelector('#id');
 const lastNameSearch = document.querySelector('#lastNameSearch');
+const lastName = document.querySelector('#lastName');
+const address = document.querySelector('#address');
 const citySearch = document.querySelector('#lastNameSearch');
 const streetSearch = document.querySelector('#streetSearch');
 const numberSearch = document.querySelector('#numberSearch');
@@ -272,16 +366,20 @@ showUserById.onclick = () => {
 
 searchBtn.onclick = () => {
     // search();
-    if (idSearch.checked)
-        s.printUsers(s.Search(parseInt(idSearch.value)));
-    if (firstNameSearch.checked)
-        s.printUsers(s.Search(firstNameSearch.value));
-    if (lastNameSearch.checked)
-        s.printUsers(s.Search(lastNameSearch));
-    if (citySearch.checked)
-        s.printUsers(s.Search(citySearch.value));
-    if (streetSearch.checked)
-        s.printUsers(s.Search(streetSearch));
-    if (numberSearch.checked)
-        s.printUsers(s.Search(numberSearch.value));
+    m = new Manager();
+    if (id.checked)
+        m.Search(parseInt(idSearch.value));
+    if (firstName.checked)
+         m.Search(firstNameSearch.value);
+    if (lastName.checked)
+        m.Search(lastNameSearch.value);
+    if (address.checked) {
+        if (citySearch.value)
+            m.Search(citySearch.value);
+        if (streetSearch.value)
+            m.Search(streetSearch);
+        if (numberSearch.value)
+            m.Search(numberSearch.value);
+    }
 }
+
