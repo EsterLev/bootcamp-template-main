46206378335041235b7fbb06c6807938e9f35acb
EstiLev <st3281357@gmail.com>
2022-06-28 14:36:38 +0300

    wedotogether

--- src/manageDairy.js
+++ src/manageDairy.js
@@ -5,7 +5,28 @@ const btnSave = document.getElementById('btnSave');
 
 const searchURL = new URLSearchParams(location.search);
 const userURL = parseInt(searchURL.get('id'));
-const daily=new Array();
+
+const getJson = () => {
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", './users.json');
+    xhr.send();
+    xhr.onload = function () {
+        if (xhr.status != 200) {
+            alert(`Error ${xhr.status}: ${xhr.statusText}`);
+        } else {
+            usersList.users = JSON.parse(xhr.responseText).users;
+            usersList.manager = JSON.parse(xhr.responseText).manager;
+        }
+    }
+}
+getJson();
+
+const usersList = {
+    manager: {},
+    users: {},
+};
+
+const daily = new Array();
 btnEdit.onclick = (e) => {
     const div = document.createElement('div');
     const desc = document.createElement('span');
@@ -16,11 +37,14 @@ btnEdit.onclick = (e) => {
     div.append(desc);
     div.append(inputDesc);
     addFoods.append(div);
-    inputDesc.onchange=()=>{
+    inputDesc.onchange = () => {
         daily.push(inputDesc.value);
     }
 }
 
-btnSave.onclick=()=>{
-    console.log(daily);
+btnSave.onclick = () => {
+    usersList.users.forEach(user => {
+        if (user.id === userURL)
+            user.managerDaily = daily;
+    });
 }
